8f7868904b0a97dc78d58714f2e8740bd68413dd
Rockie Yang <rockie.yang@ericsson.com>
2015-10-21 16:56:29 +0200

    something could work

---
+++ app.js
@@ -0,0 +1,19 @@
+var App = angular.module('drag-and-drop', ['ngDragDrop']);
+
+App.controller('oneCtrl', function($scope, $timeout, $filter) {
+  $scope.filterIt = function() {
+    return $filter('orderBy')($scope.list2, 'title');
+  };
+
+  $scope.list1 = [];
+  $scope.list2 = [
+    { 'title': 'Item 3', 'drag': true },
+    { 'title': 'Item 2', 'drag': true },
+    { 'title': 'Item 1', 'drag': true },
+    { 'title': 'Item 4', 'drag': true }
+  ];
+
+  angular.forEach($scope.list2, function(val, key) {
+    $scope.list1.push({});
+  });
+});
